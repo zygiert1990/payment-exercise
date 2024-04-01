67d528237a7ab0f3b6576e1b617a59d841b51b9b
michal.zyga <michal.zyga03@gmail.com>
2024-04-01 08:44:52 +0200

    cleanup

--- src/main/java/com/verestro/exercise/payment/controller/RegistrationController.java
+++ src/main/java/com/verestro/exercise/payment/controller/RegistrationController.java
@@ -1,30 +1,24 @@
 package com.verestro.exercise.payment.controller;
 
-import com.verestro.exercise.payment.model.UserDTO;
 import com.verestro.exercise.payment.model.UserId;
 import com.verestro.exercise.payment.model.UserRegistrationDTO;
 import com.verestro.exercise.payment.service.UserRegistrationService;
-import com.verestro.exercise.payment.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class RegistrationController {
 
-    private final UserService userService;
     private final UserRegistrationService userRegistrationService;
 
     @PostMapping(value = "register")
     public ResponseEntity<UserId> register(@RequestBody @Valid UserRegistrationDTO user) {
         return ResponseEntity.ok(userRegistrationService.register(user));
-    }
-
-    @GetMapping(value = "user/{username}")
-    public ResponseEntity<UserDTO> get(@PathVariable("username") String username) {
-        return ResponseEntity.ok(userService.findByUsername(username));
     }
 
 }
